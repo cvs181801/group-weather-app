7ebb1ab6731c21066d43e20a3e4fa28b1b7cb356
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-12 17:18:18 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//https://apis.scrimba.com/openweathermap/data/2.5/weather?q=portland
+
+//This is the scrimba weather api!  There is a lesson on using this in the frontend path module 7, lesson 22. 
+//the city at the end of this query string can be changed to any city. Response format: JSON
